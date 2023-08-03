51d33f83477e4a0486e6dbfcef2e6a9ab4a1a47b
Rafal Hes <rafal.hes@yahoo.com>
2023-08-03 15:56:23 +0200

    Add background to movie-details page.

--- js/script.js
+++ js/script.js
@@ -85,6 +85,7 @@ const displayMovieDetails = async () => {
 
   const movie = await fetchAPIData(`movie/${movieId}`);
   console.log(movie);
+  displayMovieBackground('movie', movie.backdrop_path);
   const div = document.createElement('div');
   div.innerHTML = `
       <div class="details-top">
@@ -177,6 +178,27 @@ const hideSpinner = () => {
 const addCommasToNumber = (number) => {
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
+
+const displayMovieBackground = (type, backdropPath) => {
+  const overlayDiv = document.createElement('div');
+  overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${backdropPath})`
+  overlayDiv.style.backgroundSize = 'cover';
+  overlayDiv.style.backgroundPosition = 'center';
+  overlayDiv.style.backgroundRepeat = 'no-repeat';
+  overlayDiv.style.height = '100vh';
+  overlayDiv.style.width = '100vw';
+  overlayDiv.style.position = 'absolute';
+  overlayDiv.style.top = '0';
+  overlayDiv.style.left = '0';
+  overlayDiv.style.zIndex = '-1';
+  overlayDiv.style.opacity = '0.3';
+
+  if (type === 'movie') {
+    document.querySelector('#movie-details').appendChild(overlayDiv)
+  } else {
+    document.querySelector('#tv-details').appendChild(overlayDiv);
+  }
+}
 
 // Init App
 const init = () => {
